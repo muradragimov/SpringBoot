0a94adfb7df160993f22a18b074bafb51bb6d011
Murad Ragimov <100687113+muradragimov@users.noreply.github.com>
2023-10-04 23:52:40 +0400

    Update MapperApplication.java

--- src/main/java/com/example/mapper/MapperApplication.java
+++ src/main/java/com/example/mapper/MapperApplication.java
@@ -7,10 +7,5 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MapperApplication {
-	public static void main(String[] args) {
-		SpringApplication.run(MapperApplication.class, args);
-
-
-	}
-
+       public static void main(String[] args) { SpringApplication.run(MapperApplication.class, args);}
 }
